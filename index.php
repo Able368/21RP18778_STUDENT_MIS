97eaad2517affb6e7ff078ff1e3e0ca5c0e52325
Able368 <abelniyonshuti3@gmail.com>
2025-11-05 11:32:08 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// src/index.php
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Students MIS HOME</title>
+</head>
+<body>
+  <h1>Welcome to Students MIS</h1>
+  <ul>
+    <li><a href="registration.php">Register</a></li>
+  </ul>
+</body>
+</html>
